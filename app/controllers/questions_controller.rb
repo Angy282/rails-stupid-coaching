db7620c8977b9bd5e5658ea1ceb04eb8bf3137be
Angy Hijaze <angy.hijaze@gmail.com>
2024-11-25 15:18:25 +0100

    creates responses to stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,18 @@ class QuestionsController < ApplicationController
   def ask
     # empty
   end
+
+  def answer
+    @user_input = params[:input]
+
+    if @user_input.blank?
+      @response = "Please ask a question."
+    elsif @user_input.downcase == "i am going to work"
+      @response = "Great!"
+    elsif @user_input.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
 end
